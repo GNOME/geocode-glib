cf91434516e970bac4c4ea4c3b66db1d4ea2b142
Bastien Nocera <hadess@hadess.net>
2011-05-09 15:58:52 +0100

    lib: Add defines for possible responses

--- geocode-glib/geocode-glib.h
+++ geocode-glib/geocode-glib.h
@@ -50,6 +50,210 @@ struct GeocodeObjectClass {
 	GObjectClass parent_class;
 };
 
+/* Known metadata fields */
+
+/**
+ * GEOCODE_OBJECT_FIELD_LONGITUDE:
+ *
+ * Metadata field for a result, representing the longitude
+ * at the center of the matched area.
+ **/
+#define GEOCODE_OBJECT_FIELD_LONGITUDE		"longitude"
+
+/**
+ * GEOCODE_OBJECT_FIELD_LATITUDE:
+ *
+ * Metadata field for a result, representing the latitude
+ * at the center of the matched area.
+ **/
+#define GEOCODE_OBJECT_FIELD_LATITUDE		"latitude"
+
+/**
+ * GEOCODE_OBJECT_FIELD_RADIUS:
+ *
+ * Metadata field for a result, representing the radius of the
+ * matched area in meters.
+ **/
+#define GEOCODE_OBJECT_FIELD_RADIUS		"radius"
+
+/**
+ * GEOCODE_OBJECT_FIELD_TIMEZONE:
+ *
+ * Metadata field for a result, representing the timezone
+ * of the matched area.
+ **/
+#define GEOCODE_OBJECT_FIELD_TIMEZONE		"timezone"
+
+/**
+ * GEOCODE_OBJECT_FIELD_CITY:
+ *
+ * Metadata field for a result, representing the city
+ * of the matched area.
+ **/
+#define GEOCODE_OBJECT_FIELD_CITY		"city"
+
+/**
+ * GEOCODE_OBJECT_FIELD_COUNTRY:
+ *
+ * Metadata field for a result, representing the country
+ * of the matched area.
+ **/
+#define GEOCODE_OBJECT_FIELD_COUNTRY		"country"
+
+/**
+ * GEOCODE_OBJECT_FIELD_AIRPORT:
+ *
+ * Metadata field for a result, representing the closest
+ * airport to the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_AIRPORT		"airport"
+
+/**
+ * GEOCODE_OBJECT_FIELD_UZIP:
+ *
+ * Metadata field for a result, representing the unique
+ * zip code (or postal code) for the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_UZIP		"uzip"
+
+/**
+ * GEOCODE_OBJECT_FIELD_COUNTRYCODE:
+ *
+ * Metadata field for a result, representing the country
+ * code of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_COUNTRYCODE	"countrycode"
+
+/**
+ * GEOCODE_OBJECT_FIELD_NAME:
+ *
+ * Metadata field for a result, representing the name
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_NAME		"name"
+
+/**
+ * GEOCODE_OBJECT_FIELD_LINE1:
+ *
+ * Metadata field for a result, representing the line1
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_LINE1		"line1"
+
+/**
+ * GEOCODE_OBJECT_FIELD_LINE2:
+ *
+ * Metadata field for a result, representing the line2
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_LINE2		"line2"
+
+/**
+ * GEOCODE_OBJECT_FIELD_LINE3:
+ *
+ * Metadata field for a result, representing the line3
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_LINE3		"line3"
+
+/**
+ * GEOCODE_OBJECT_FIELD_LINE4:
+ *
+ * Metadata field for a result, representing the line4
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_LINE4		"line4"
+
+/**
+ * GEOCODE_OBJECT_FIELD_HOUSE:
+ *
+ * Metadata field for a result, representing the house
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_HOUSE		"house"
+
+/**
+ * GEOCODE_OBJECT_FIELD_STREET:
+ *
+ * Metadata field for a result, representing the street
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_STREET		"street"
+
+/**
+ * GEOCODE_OBJECT_FIELD_POSTAL:
+ *
+ * Metadata field for a result, representing the postal
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_POSTAL		"postal"
+
+/**
+ * GEOCODE_OBJECT_FIELD_NEIGHBORHOOD:
+ *
+ * Metadata field for a result, representing the neighborhood
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_NEIGHBORHOOD	"neighborhood"
+
+/**
+ * GEOCODE_OBJECT_FIELD_COUNTY:
+ *
+ * Metadata field for a result, representing the county
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_COUNTY		"county"
+
+/**
+ * GEOCODE_OBJECT_FIELD_HASH:
+ *
+ * Metadata field for a result, representing the hash
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_HASH		"hash"
+
+/**
+ * GEOCODE_OBJECT_FIELD_STATECODE:
+ *
+ * Metadata field for a result, representing the statecode
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_STATECODE		"statecode"
+
+/**
+ * GEOCODE_OBJECT_FIELD_STATE:
+ *
+ * Metadata field for a result, representing the state
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_STATE		"state"
+
+/**
+ * GEOCODE_OBJECT_FIELD_COUNTYCODE:
+ *
+ * Metadata field for a result, representing the county code
+ * of the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_COUNTYCODE		"countycode"
+
+/**
+ * GEOCODE_OBJECT_FIELD_OFFSETLAT:
+ *
+ * Metadata field for a result, representing the latitude of
+ * the offset point in degrees, representing building/parcel
+ * setback from road center-line for the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_OFFSETLAT		"offsetlat"
+
+/**
+ * GEOCODE_OBJECT_FIELD_OFFSETLON:
+ *
+ * Metadata field for a result, representing the longitude
+ * the offset point in degrees, representing building/parcel
+ * setback from road center-line for the matched area.
+**/
+#define GEOCODE_OBJECT_FIELD_OFFSETLON		"offsetlon"
+
 GeocodeObject *geocode_object_new (void);
 GeocodeObject *geocode_object_new_for_params (GHashTable *params);
 GeocodeObject *geocode_object_new_for_coords (gdouble     latitude,
